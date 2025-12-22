9d984f4722cd2a9173e1b0a072387168ff1ffc32
Lorenzo <lorenzocaruso999@gmail.com>
2025-12-22 15:51:57 +0100

    VersioneFinale

--- src/main/java/com/example/smartlibrary/repository/BookRepository.java
+++ src/main/java/com/example/smartlibrary/repository/BookRepository.java
@@ -15,7 +15,11 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Query("""
             SELECT b FROM Book b
-            WHERE (:title IS NULL OR LOWER(b.title) LIKE LOWER(CONCAT('%', :title, '%')))
+            WHERE (
+                    :title IS NULL
+                    OR LOWER(b.title)  LIKE LOWER(CONCAT('%', :title, '%'))
+                    OR LOWER(b.author) LIKE LOWER(CONCAT('%', :title, '%'))
+                  )
               AND (:author IS NULL OR LOWER(b.author) LIKE LOWER(CONCAT('%', :author, '%')))
               AND (:genre IS NULL OR LOWER(b.genre) LIKE LOWER(CONCAT('%', :genre, '%')))
               AND (:publicationYear IS NULL OR b.publicationYear = :publicationYear)
